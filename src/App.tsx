a0bfd70a1dad3e977df081109523b777b9095712
hb-17 <softdevhasan@gmail.com>
2024-07-10 16:17:17 +0700

    7. membuat home page - library service component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar'
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks'
 import { Carousel } from './layouts/HomePage/Carousel'
 import { Heros } from './layouts/HomePage/Heros'
+import { LibraryService } from './layouts/HomePage/LibraryService'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <ExploreTopBooks />
       <Carousel />
       <Heros />
+      <LibraryService />
     </div>
   )
 }
