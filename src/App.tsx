35d6ef1434ab33df60c120da3993e6644a258f0f
hb-17 <softdevhasan@gmail.com>
2024-07-17 10:13:43 +0700

    install routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Navbar } from './layouts/NavbarAndFooter/Navbar'
 import { Footer } from './layouts/NavbarAndFooter/Footer'
 import { HomePage } from './layouts/HomePage/HomePage'
+import { Route } from 'react-router-dom';
+import { SearchBookPage } from './layouts/SearchBookPage/SearchBookPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,14 @@ function App() {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      <Route path='/'>
+        <HomePage />
+      </Route>
+      
+      <Route path='/search'>
+        <SearchBookPage />
+      </Route>
+
       <Footer />
     </div>
   )
